5f258fbf381025db3e0344681076df59ff3e9c46
edureka <salehakhans@gmail.com>
2024-05-23 08:55:15 +0000

    First commit of Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,17 @@
+//this is a simple java program
+//file name = Helloworld.java
+
+
+class Helloworld
+{
+	public static void main (string args[])
+	{
+		system.out.println("Hello Shine");
+	}
+
+}
+
+
+
+
+
